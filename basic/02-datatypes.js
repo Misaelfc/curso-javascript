ba500e660e32943266ae4a59e79d3e71d3fac5f6
AbrahamDev <aflorescastrejon@gmail.com>
2024-08-22 12:39:14 -0500

    Se agregan los ficheros a la carpeta basic y se crea el fichero 03-beginner-excercises

---
+++ basic/02-datatypes.js
@@ -0,0 +1,53 @@
+// Tipos de datos primitivos
+
+// String (cadena de texto)
+let myName = "Abraham Flores"
+let alias = 'AbrahamDev'
+let email = `aflorescastrejon@gmail.com`
+
+// Números
+let age = 33 // Entero
+let height = 1.80 // Decimal
+
+// Booleanos(boolean)
+let = isStudent = true
+let isTeacher = false
+
+// Undefined
+let UndefinedValue
+console.log(UndefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(6349264823628145376)
+let myBigInt2 = 624963246832648231126287n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof UndefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
+
+
+
+
+
