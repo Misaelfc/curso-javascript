7a45a1c414bd656f0a2d69dc28b021899dec015f
AbrahamDev <aflorescastrejon@gmail.com>
2024-08-25 00:58:13 -0500

    Se hace el fichero 04 operators y se modifica el fichero 03

---
+++ basic/04-operators.js
@@ -0,0 +1,40 @@
+// Operadores
+
+// Operadores Aritméticos
+
+let a = 5
+let b = 10
+
+console.log(a + b) //Suma
+console.log(a - b) //Resta
+console.log(a * b) //Multiplicación
+console.log(a / b) //División
+
+console.log(a % b) //Módulo
+console.log(a ** b) //Exponente
+
+a++ //Incremento
+console.log(a)
+
+b-- //Decremento
+console.log(b)
+
+// Operadores de asignación
+
+let myVariable = 2
+console.log(myVariable)
+myVariable += 2
+console.log(myVariable)
+
+myVariable -= 2
+myVariable *= 2
+myVariable /= 2
+myVariable %= 2
+myVariable **= 2
+
+// Operadores de Comparación
+
+console.log(a > b)
+console.log(a < b)
+console.log(10 >= 10)
+console.log(a <= b)
