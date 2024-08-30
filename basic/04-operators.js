1a13068d431cb8086150d8f59a6888f8f56232dc
AbrahamDev <aflorescastrejon@gmail.com>
2024-08-29 21:29:38 -0500

    Se realiza el fichero número 03 de ejercicios y se añaden los ficheros 05 de operators-excercises y el 06 de strings

--- basic/04-operators.js
+++ basic/04-operators.js
@@ -38,3 +38,64 @@ console.log(a > b)
 console.log(a < b)
 console.log(10 >= 10)
 console.log(a <= b)
+console.log(a == b)
+console.log(a == 6) //Igualdad por valor
+console.log(a == "6") //Igualdad por valor
+console.log(a == a)
+console.log(a === a) //Igualdad por identidad(por tipo y valor)
+console.log(a === 6) 
+console.log(a === "6")
+console.log(a != 6)
+console.log(a !== "6")
+console.log(0 == false)
+console.log(1 == false)
+console.log(2 == false)
+console.log(0 == "")
+console.log(0 == " ")
+console.log(0 == ``)
+console.log(0 == "Hola")
+console.log(0 === "")
+console.log(undefined == null)
+console.log(undefined === null)
+
+// Truthy values( valores verdaderos)
+
+// Todos los números positivos y negativos menos el cero
+// Todas las cadenas de texto menos las vacías
+// El boolean true
+// Falsy values( valores falsos)
+
+// 0
+// 0n
+// null
+// undefined
+// NaN
+// El boolean false
+//Cadenas de texto vacías
+
+// Operadores lógicos
+
+// and(&&)
+console.log(5 > 10 && 15 > 20)
+console.log(5 < 10 && 15 < 20)
+console.log(5 < 10 && 15 > 20)
+console.log(5 > 10 && 15 > 20 && 30 > 40)
+
+// or(||)
+console.log(5 > 10 || 15 > 20)
+console.log(5 < 10 || 15 < 20)
+console.log(5 < 10 || 15 > 20)
+console.log(5 < 10 || 15 > 20 || 30 > 40)
+
+console.log(5 < 10 && 15 > 20 || 30 < 40)
+
+// not(!)
+console.log(!true)
+console.log(!false)
+console.log(!(5 > 10 && 15 > 20))
+console.log(!(5 > 10 || 15 > 20))
+
+// Operadores ternarios
+const isRaining = false
+
+isRaining ? console.log("Está lloviendo") : console.log("No está lloviendo")
